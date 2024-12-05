b839f1e73460a206129134ab98d8a94fa241f25c
SHIN SUJUNG <shinssjj79@gmail.com>
2024-12-05 17:54:42 +0900

    Initial commit

---
+++ src/main/java/com/example/rockpaperscissorsP/RockpaperscissorsPApplication.java
@@ -0,0 +1,13 @@
+package com.example.rockpaperscissorsP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RockpaperscissorsPApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RockpaperscissorsPApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/rockpaperscissorsP/RockpaperscissorsPApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rockpaperscissorsP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RockpaperscissorsPApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
